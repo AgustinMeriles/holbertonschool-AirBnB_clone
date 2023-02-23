d00b9c3ed98cd789b84b0b471444ec040b0bae18
agustinmeriles <agustinmeriles@hotmail.com>
2023-02-23 10:22:47 -0300

    models/base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Shebang"""
 import uuid
-from datetime import datetime
+import datetime
 
 
 class BaseModel:
@@ -11,15 +11,15 @@ class BaseModel:
         """Constructor of the class"""
 
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.update_at = datetime.now()
+        self.created_at = datetime.datetime.now()
+        self.update_at = self.created_at
 
     def __str__(self):
         return (f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
         """Updates the attribute with the current datetime"""
-        self.update_at = datetime.now()
+        self.update_at = datetime.datetime.now()
 
     def to_dict(self):
         newDict = self.__dict__.copy()
