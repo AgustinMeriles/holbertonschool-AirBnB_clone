bd1d350f9b899b46eb5bdfc44f573e4a03cc5333
agustinmeriles <agustinmeriles@hotmail.com>
2023-02-23 11:10:38 -0300

    hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -12,16 +12,14 @@ class BaseModel:
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        self.update_at = self.created_at
+        self.updated_at = self.created_at
 
     def __str__(self):
         return (f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
 
-    def save(self, name, value):
+    def save(self):
         """Updates the attribute with the current datetime"""
-        if name != self.update_at:
-            self.update_at = datetime.datetime.now()
-        super().__setattr__(name, value)
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         newDict = self.__dict__.copy()
