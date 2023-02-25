80272544368a2e5fff04451d2a971c097cc1dcfe
Agus <agustinmeriles@hotmail.com>
2023-02-25 19:30:09 -0300

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,17 +2,28 @@
 """Shebang"""
 import uuid
 import datetime
+from models import storage
 
 
 class BaseModel:
     """Initialization of class BaseModel"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor of the class"""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            self.created_at = datetime.datetime.strptime(
+                kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.datetime.strptime(
+                kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         return (f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
@@ -20,6 +31,7 @@ class BaseModel:
     def save(self):
         """Updates the attribute with the current datetime"""
         self.updated_at = datetime.datetime.now()
+        storage.save()
 
     def to_dict(self):
         newDict = self.__dict__.copy()
@@ -27,13 +39,3 @@ class BaseModel:
         newDict["created_at"] = self.created_at.isoformat()
         newDict["updated_at"] = self.updated_at.isoformat()
         return newDict
-
-    def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
