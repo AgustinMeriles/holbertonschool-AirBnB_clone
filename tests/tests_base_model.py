890149073f80a6e1910546019b4570fb2e62fbdc
NexoNeko <meresidamente@hotmail.com>
2023-02-28 16:48:15 -0300

    Added specific module tests

--- tests/tests_base_model.py
+++ tests/tests_base_model.py
@@ -2,31 +2,39 @@
 """unittests for base_model.py"""
 import unittest
 from models.base_model import BaseModel
+from models import storage
 
 
-class test_base_basics(unittest.TestCase):
+class tests_base(unittest.TestCase):
+
     def test_string(self):
+        """ Testing manual name setup for
+        attribute """
         my_model = BaseModel()
         my_model.name = "Something"
         self.assertEqual(my_model.name, "Something")
 
     def test_timestamp_create(self):
+        """ Testing timestamp creation """
         my_model = BaseModel()
         self.assertIsNotNone(my_model.created_at)
 
     def test_timestamp_update(self):
+        """ Testing timestamp update on save """
         my_model = BaseModel()
         my_model.save()
         self.assertNotEqual(my_model.created_at, my_model.updated_at)
 
-
-class test_base_dict(unittest.TestCase):
     def test_dict(self):
+        """ Testing to dictionary function correct
+        type"""
         my_model = BaseModel()
         my_model_dict = my_model.to_dict()
         self.assertEqual(str(type(my_model_dict)), "<class 'dict'>")
 
     def test_recreation(self):
+        """ Testing recreation of base model
+        from saved json """
         my_model = BaseModel()
         my_model.name = "My_First_Model"
         my_model_json = my_model.to_dict()
@@ -34,6 +42,8 @@ class test_base_dict(unittest.TestCase):
         self.assertEqual(my_model.name, my_new_model.name)
 
     def test_recreation_datetype(self):
+        """ Testing recreation of base model from
+        saved json, timestamp """
         my_model = BaseModel()
         my_model_json = my_model.to_dict()
         my_new_model = BaseModel(**my_model_json)
@@ -41,6 +51,11 @@ class test_base_dict(unittest.TestCase):
         print(type(my_new_model.created_at))
         self.assertEqual(type(my_model.created_at),type(my_new_model.created_at))
 
+    def test_id(self):
+        """ Testing if IDs are unique """
+        my_model = BaseModel()
+        my_model_a = BaseModel()
+        self.assertNotEqual(my_model.id, my_model_a.id)
 
 if __name__ == "__main__":
     unittest.main()
