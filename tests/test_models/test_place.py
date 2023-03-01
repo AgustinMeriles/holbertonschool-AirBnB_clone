8f7ce7463bfc3f06ed4f34d9c8706907c5adaf7d
NexoNeko <meresidamente@hotmail.com>
2023-02-28 21:32:56 -0300

    BUgfix

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -8,51 +8,10 @@ Modules tested:
 - Review
 - State """
 import unittest
-from models.user import User
-from models.city import City
-from models.amenity import Amenity
 from models.place import Place
-from models.review import Review
-from models.state import State
-
 
 class Tests(unittest.TestCase):
     """ Test functions for modules"""
-
-    def test_user_first_name(self):
-        """ Testing User attributes"""
-        var = User()
-        self.assertEqual(var.first_name, "")
-
-    def test_user_last_name(self):
-        """ Testing User attributes"""
-        var = User()
-        self.assertEqual(var.last_name, "")
-
-    def test_user_email(self):
-        """ Testing User attributes"""
-        var = User()
-        self.assertEqual(var.email, "")
-
-    def test_user_password(self):
-        """ Testing User attributes"""
-        var = User()
-        self.assertEqual(var.password, "")
-
-    def test_city_state_id(self):
-        """ Testing City attributes """
-        var = City()
-        self.assertEqual(var.state_id, "")
-
-    def test_city_name(self):
-        """ Testing City attributes """
-        var = City()
-        self.assertEqual(var.name, "")
-
-    def test_amenity(self):
-        """ Testing amenity attributes """
-        var = Amenity()
-        self.assertEqual(var.name,"")
 
     def test_place_city_id(self):
         """ Testing Place attributes """
@@ -108,23 +67,3 @@ class Tests(unittest.TestCase):
         """ Testing Place attributes """
         var = Place()
         self.assertEqual(var.amenity_ids, [])
-
-    def test_review_place_id(self):
-        """ Testing Review attributes """
-        var = Review()
-        self.assertEqual(var.place_id, "")
-
-    def test_review_user_id(self):
-        """ Testing Review attributes """
-        var = Review()
-        self.assertEqual(var.user_id, "")
-
-    def test_review_text(self):
-        """ Testing Review attributes """
-        var = Review()
-        self.assertEqual(var.text, "")
-
-    def test_state(self):
-        """ Testing State attributes """
-        var = State()
-        self.assertEqual(var.name, "")
